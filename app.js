acf36100a2e2457984a6eb01d36a6b72367a4af4
Alex <iz-nino@yandex.ru>
2024-05-26 20:51:20 +0300

    new

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 let tg = window.Telegram.WebApp;
 
+tg.expand();
+tg.MainButton.show();
+tg.MainButton.setText("Привет");
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+
 
 
